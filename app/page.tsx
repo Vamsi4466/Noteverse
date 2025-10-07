520a5f5f6dad91d7ccfe7ad0a07f37011849e289
Vamsikrishna Setty <settyvamsikrishna83@gmail.com>
2025-10-07 18:46:21 +0530

    resolved merge conflicts

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import { Button } from "@/components/ui/button";
 import Image from "next/image";
 import Header from "./_components/Header";
@@ -9,23 +8,6 @@ export default function Home() {
     <div className="">
       <Header />
       <Hero />
-=======
-"use client"
-import Image from "next/image";
-import Header from "./_components/Header";
-import Hero from "./_components/Hero";
-import { useKindeBrowserClient } from "@kinde-oss/kinde-auth-nextjs";
-import { useEffect } from "react";
-
-export default function Home() {
-
-  const {user}=useKindeBrowserClient();
-  
-  return (
-    <div>
-      <Header/>
-      <Hero/>
->>>>>>> 06aa319 (added editor and canvas functionalities)
     </div>
   );
 }
--- middleware.ts
+++ middleware.ts
@@ -1,6 +1,5 @@
 import { NextResponse } from 'next/server'
 import type { NextRequest } from 'next/server'
-<<<<<<< HEAD
 import { getKindeServerSession } from '@kinde-oss/kinde-auth-nextjs/server'
 
 export async function middleware(request: NextRequest) {
@@ -21,20 +20,3 @@ export async function middleware(request: NextRequest) {
 export const config = {
   matcher: ['/dashboard/:path*'],
 }
-=======
-import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
-// This function can be marked `async` if using `await` inside
-export async function middleware(request: NextRequest) {
-    const { isAuthenticated } = getKindeServerSession();
-    if(!await isAuthenticated())
-    {
-        return NextResponse.redirect(new URL('/api/auth/login?post_login_redirect_url=/dashboard', request.url))
-
-    }
-}
- 
-// See "Matching Paths" below to learn more
-export const config = {
-  matcher: ['/dashboard'],
-}
->>>>>>> 06aa319 (added editor and canvas functionalities)
